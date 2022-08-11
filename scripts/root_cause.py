b49a73c3d02c89dade5eabc5f287aaeb197a8530
Zehao Wang <zhwang110@gmail.com>
2022-08-10 21:41:15 -0400

    minor

--- scripts/root_cause.py
+++ scripts/root_cause.py
@@ -7,7 +7,7 @@ nouse_second=['Serialization', 'Logging', 'Other', 'Spark Bug']
 df = df[(True ^ df['Classification'].isin(nouse_second))]
 print(len(df))
 # get the percentage for root cause
-classify_type = 'root cause'
+classify_type = 'Root Cause'
 total = pd.DataFrame({'total': df.groupby(classify_type).size()})
 total = total.sort_values(['total'], ascending=False)
 total['number'] = total['total']
